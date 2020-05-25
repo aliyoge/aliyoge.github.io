dd6e4ea226b7705dec2a651ae5a9a015759f703a
wenjunhuang <kongkongss@yeah.net>
2020-05-25 20:50:27 +0800

    remove env

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,14 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "781934c571cefeaa836a93f870935762",
 "/": "781934c571cefeaa836a93f870935762",
-"main.dart.js": "ea27ef1dc7e44b8b3797e352fac16d0c",
+"main.dart.js": "c539b1a4129f384c3301b3c787528fab",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "33e1345cf3424c6397e954c4e40d7866",
-"assets/LICENSE": "50386fba5ea48d6c7b575ade4e00ef64",
-"assets/AssetManifest.json": "e488f8078e77d7b4d008006fdcb03993",
+"assets/LICENSE": "e1094d7586c8f355a961b2c9d3216383",
+"assets/AssetManifest.json": "18bb85605817b5b8ca48b5dad63bdc25",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
